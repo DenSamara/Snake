30097424460ebed75a7b6cbbfcb218fa8ae95c1f
Denis Konovalov <den.konovaloff@gmail.com>
2018-09-14 16:14:36 +0400

    Lesson 7

---
+++ Snake/HorizontalLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+    class HorizontalLine : Line
+    {
+        public HorizontalLine(int x, int y, int length, char sym)
+        {
+            this.sym = sym;
+            for (int i = 0; i < length; i++)
+            {
+                pList.Add(new Point(x+i, y, sym));
+            }
+        }
+    }
+}
---
+++ Snake/Line.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+    public class Line
+    {
+        protected char sym = '*';
+        protected List<Point> pList;
+
+        public Line()
+        {
+            pList = new List<Point>();
+        }
+
+        public Line(Point start, Point end) : this()
+        {
+            for(int i = start.X; i <= end.X; i++ ){
+                for (int j = start.Y; j <= end.Y; j++)
+                {
+                    pList.Add(new Point(i, j, '*'));
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point item in pList)
+            {
+                item.Draw();
+            }
+        }
+    }
+}
